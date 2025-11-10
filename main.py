afac7b58fb6a7801a5e706e56c1a1da366d91227
Grigory Bordyugov <gregbg@gmail.com>
2025-11-10 11:05:43 +0100

    add datastructures and cards

--- main.py
+++ main.py
@@ -1,3 +1,48 @@
+from dataclasses import dataclass
+from enum import Enum
+
+
+class Colour(Enum):
+    RED = 'red'
+    GREEN = 'green'
+    BLUE = 'blue'
+
+
+class Shape(Enum):
+    OVAL = 'oval'
+    RHOMBUS = 'rhombus'
+    WORM = 'worm'
+
+
+class Number(Enum):
+    ONE = 1
+    TWO = 2
+    THREE = 3
+
+
+class Pattern(Enum):
+    FILLED = 'filled'
+    HATCHED = 'hatched'
+    BLANK = 'blank'
+
+
+@dataclass
+class Card:
+    colour: Colour
+    shape: Shape
+    number: Number
+    pattern: Pattern
+
+
+cards = [
+    Card(colour=c, shape=s, number=n, pattern=p)
+    for c in Colour
+    for s in Shape
+    for n in Number
+    for p in Pattern
+]
+
+
 def main():
     print("Hello from set!")
 
