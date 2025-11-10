cec871a9523e9df8dbdf4102f9611313b2a7d399
Grigory Bordyugov <gregbg@gmail.com>
2025-11-10 12:44:20 +0100

    implement brute force search

--- main.py
+++ main.py
@@ -1,32 +1,32 @@
 from dataclasses import dataclass
-from enum import Enum
+from enum import IntEnum, StrEnum
 
 
-class Colour(Enum):
+class Colour(StrEnum):
     RED = 'red'
     GREEN = 'green'
     PURPLE = 'purple'
 
 
-class Shape(Enum):
+class Shape(StrEnum):
     DIAMOND = 'diamond'
     SQUIGGLE = 'squiggle'
     OVAL = 'oval'
 
 
-class Number(Enum):
+class Number(IntEnum):
     ONE = 1
     TWO = 2
     THREE = 3
 
 
-class Shading(Enum):
+class Shading(StrEnum):
     SOLID = 'solid'
     STRIPED = 'striped'
-    OPEN = 'OPEN'
+    OPEN = 'open'
 
 
-@dataclass(frozen=True)
+@dataclass(frozen=True, order=True)
 class Card:
     colour: Colour
     shape: Shape
@@ -45,14 +45,26 @@ cards = [
 
 def is_set(c1: Card, c2: Card, c3: Card) -> bool:
     cards = {c1, c2, c3}
+
     colours = {c.colour for c in cards}
     shapes = {c.shape for c in cards}
     numbers = {c.number for c in cards}
     shadings = {c.shading for c in cards}
 
     properties = [colours, shapes, numbers, shadings]
-
     return all(len(p) == 1 or len(p) == 3 for p in properties)
+
+
+def find_sets_brute_force(cards: set[Card]) -> list[set[Card]]:
+    assert len(cards) >= 3, "Need at least three cards."
+    triples = [
+        {c1, c2, c3}
+        for c1 in cards
+        for c2 in cards
+        for c3 in cards
+        if c1 < c2 < c3
+    ]
+    return [t for t in triples if is_set(*t)]
 
 
 def main():
